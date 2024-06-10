a1c9a72415d24c16b9634a967a9224f3b08adb61
suhwan <99950377+Armstrong-SuHwan@users.noreply.github.com>
2024-06-10 15:03:26 +0900

    Update update-user.dto.ts

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types'; // 상속받은 클래스의 모든 속성을 @IsOptional() 데코레이터 적용 상태로 변환
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {}
