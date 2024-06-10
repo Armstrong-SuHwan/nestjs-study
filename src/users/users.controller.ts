91fb216dc93f2ac0991cffebece3d3487bc385f9
suhwan <99950377+Armstrong-SuHwan@users.noreply.github.com>
2024-06-10 15:04:22 +0900

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,71 +6,30 @@ import {
   Patch,
   Param,
   Delete,
-  Res,
-  HttpCode,
   Put,
-  BadRequestException,
-  Header,
-  Redirect,
-  ParseIntPipe,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @HttpCode(202)
+  // http://localhost:3000/users?offset=10&limit=10
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  findAll(@Query() getUserDto: GetUserDto) {
+    const { offset, limit } = getUserDto;
+    return `유저 조회, offset: ${offset}, limit: ${limit}`;
   }
 
-  // Response의 Type
-  // import { Response } from 'express';
-
-  // @Get()
-  // findAll(@Res() res: Response) {
-  //   const users = this.usersService.findAll();
-  //   return res.status(202).send(users);
-  // }
-
-  // Redirect 적용
-  // 서브 라우트의 정의 순서가 컨트롤러 동작에 영향
-  @Get('nest')
-  @Redirect('https://nestjs.com', 301)
-  findAllWithRedirect() {
-    console.log('redirect controller!!!!');
-    return this.usersService.findAll();
-  }
-
-  // Custom Header 적용
-  // Nan이 Validation이 제대로 안됨
-  @Header('Custom', 'Test Header')
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    console.log('id is string type:::', id);
-    if (+id < 1) {
-      throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
-    } else {
-      console.log('id boolean', +id < 1);
-      console.log('id:::', +id);
-    }
-    return this.usersService.findOne(+id);
-  }
-
-  // Parameter Validation 적용
-  @Get(':id')
-  findOneWithHeader(@Param('id', ParseIntPipe) id: number) {
-    console.log('id is number type:::', id);
-    return this.usersService.findOne(+id);
-  }
-
+  // http://localhost:3000/users
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+    const { name, email } = createUserDto;
+    return `유저 생성, 이름: ${name}, 이메일: ${email}`;
   }
 
   @Put(':id')
