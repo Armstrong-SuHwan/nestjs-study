d0a227de5b496687cd127cecc2b8d4e8a49fd437
rysdid17 <ryu23800@gmail.com>
2024-06-16 16:36:50 +0900

    ch.5 1st commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,6 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { EmailService } from './email/email.service';
-import { ScopeController } from './scope/scope.controller';
-import { ScopeService } from './scope/scope.service';
-import { ScopeLoggerService } from './scope/scope.logger.service';
-import { ScopeAnotherService } from './scope/scope.another.service';
-import { CustomProviderModule } from './custom-provider/custom-provider.module';
-import { ScopeRequestService } from './scope/scope.request.service';
 
 // TODO. Value Provider 사용 시 주의사항
 // import { CustomProviderService } from './custom-provider/custom-provider.service';
@@ -19,19 +13,11 @@ import { ScopeRequestService } from './scope/scope.request.service';
 // };
 
 @Module({
-  imports: [CustomProviderModule],
-  controllers: [UsersController, ScopeController],
+  imports: [],
+  controllers: [UsersController],
   providers: [
     UsersService,
     EmailService,
-    ScopeService,
-    ScopeLoggerService,
-    ScopeAnotherService,
-    ScopeRequestService,
-    // {
-    //   provide: CustomProviderService,
-    //   useValue: mockCustomProviderService,
-    // },
   ],
 })
 export class AppModule {}
