dc54782016ba7af6a7b5ca731d1ab39d09104bbd
Yo! sure <showmustgoon0702@gmail.com>
2024-07-15 22:52:56 +0900

    [CH7] Pipe

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly configService: ConfigService,
-  ) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return process.env.DATABASE_HOST;
-  }
-
-  @Get('/db-host-from-config')
-  getDatabaseHostFromConfigService(): string {
-    return this.configService.get('DATABASE_HOST');
+    return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: (process.env.NODE_ENV === 'production') ? '.production.env'
-      : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.development.env'
-  })],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
